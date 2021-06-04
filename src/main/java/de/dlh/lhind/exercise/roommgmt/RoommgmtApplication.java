f37f3330d12a67f2154593b4223e9091c9db36d4
U737700 <andy.wong@lhind.dlh.de>
2021-06-04 02:22:30 +0200

    - test commit

--- src/main/java/de/dlh/lhind/exercise/roommgmt/RoommgmtApplication.java
+++ src/main/java/de/dlh/lhind/exercise/roommgmt/RoommgmtApplication.java
@@ -3,6 +3,7 @@ package de.dlh.lhind.exercise.roommgmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Test
 @SpringBootApplication
 public class RoommgmtApplication {
 
